ec0b5cb9259f0f768d171ac34a41caf4048480a5
Felipe Palagio <115948433+FelipePalagio@users.noreply.github.com>
2026-02-25 20:25:47 +0000

    Initialize body style on DOMContentLoaded

---
+++ static/script.js
@@ -0,0 +1,18 @@
+//INICIALIZACAO DA UM ESTILO PRO FUNDO
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.style.cssText = `
+  margin: 0;
+  display: flex;
+  height: 100vh;
+  background: black;
+  color: #eee;
+  font-family: 'Inter', sans-serif;`;
+
+
+  
+
+
+
+
+});
+
